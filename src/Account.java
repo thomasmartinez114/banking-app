a4d9573aceffbd8980518af708f508f2fd00ee55
Thomas Martinez <tmartinez114@gmail.com>
2020-11-14 17:00:08 -0500

    Added Constructor and functions for Depositing and Withdrawing money

--- src/Account.java
+++ src/Account.java
@@ -1,4 +1,31 @@
+import java.util.Scanner;
 
 public class Account {
-
+	// Class variables
+	int balance;
+	int previousTransaction;
+	String customerName;
+	String customerID;
+	
+	// Constructor
+	Account(String cname, String cid) {
+		customerName = cname;
+		customerID = cid;
+	}
+	
+	// function for Depositing money
+	void deposit(int amount) {
+		if (amount != 0) {
+			balance = balance + amount;
+			previousTransaction = amount;
+		}
+	}
+	
+	// function for Withdrawing money
+	void withdraw(int amount) {
+		if (amount != 0) {
+			balance = balance - amount;
+			previousTransaction = -amount;
+		}
+	}
 }
